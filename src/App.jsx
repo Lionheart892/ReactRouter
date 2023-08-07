a81aab8176a23ce604f884fdab28e0ce68cb7931
root <root@DESKTOP-E6801AM>
2023-08-07 03:07:10 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Red from './Components/Red'
+import Blue from './Components/Blue'
+import Home from './Components/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">
+        <Link to='/home'>Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/home' element={Home}/>
+          <Route path="/blue" element={Blue}/>
+          <Route path="/red" element={Red}/>
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
